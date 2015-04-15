8461af0945c6fe23f8d424d44dd9bc0a0adf0c67
Luis Capelo <luiscape@gmail.com>
2015-04-15 02:52:44 -0400

    adding rethinkdb startup

--- setup.sh
+++ setup.sh
@@ -7,12 +7,14 @@ SCRIPTS_PATH="scripts"
 export PS1="digital-\[\e[01;36m\]\u\[\e[0m\]\[\e[00;37m\] : \W \\$ \[\e[0m\]"
 
 ## Installing database
-apt-get update
-apt-get install rethinkdb
-apt-get install screen
+sudo apt-get update -qq
+sudo apt-get install rethinkdb -y
+sudo apt-get install screen -y
 
 ## Configuring rethinkdb.
-# screen rethinkdb
+sudo rethinkdb --io-threads 2048 --daemon
+ulimit -S -n 2048
+sleep 10
 
 ## Installing Python dependencies.
 virtualenv venv
