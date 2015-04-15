b9ea700e16be7e0d537948ae1b431ef8d541d22f
Luis Capelo <luiscape@gmail.com>
2015-04-15 03:35:52 -0400

    adding sudo again

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ export PS1="digital-\[\e[01;36m\]\u\[\e[0m\]\[\e[00;37m\] : \W \\$ \[\e[0m\]"
 
 ## Installing database
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install rethinkdb
 
